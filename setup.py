a3757dd5ccf11bcaec3beec739fc9c1a84993780
Andreas Teller <andreas.teller78@gmail.com>
2019-04-19 13:23:04 +0200

    Added requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup( 
+      name='tom-sawyer-toolbox', 
+      version = '0.1', 
+      description='', 
+      author='Andreas Teller', 
+      author_email='andreas.teller@uni-jena.de', 
+      packages=['tom-sawyer-toolbox'], 
+      install_requires=[]
+     ) 
